ddc7f598f7cf9f7d02a585cb941a4364cbfe53ab
connectphome <accenturepega@gmail.com>
2017-04-05 12:47:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,11 +71,11 @@ def makeWebhookResult(device):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+  #  speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+   #          ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
-    print("Response:")
-    print(speech)
+    #print("Response:")
+   # print(speech)
 
     return {
         "speech": "You have registered " + device,
