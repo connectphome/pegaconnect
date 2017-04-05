60f79cb1bc15e4df746aef10d9002168f2aea0d8
connectphome <accenturepega@gmail.com>
2017-04-05 14:32:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") == "GoogleHome":
         speech = "Today in "
         print("Response:")
         print(speech)
@@ -48,14 +48,23 @@ def processRequest(req):
 			# "contextOut": [],
 			"source": "apiai-weather-webhook-sample"
 		}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+	elif req.get("result").get("action") == "yahooWeatherForecast":
+		baseurl = "https://query.yahooapis.com/v1/public/yql?"
+		yql_query = makeYqlQuery(req)
+		if yql_query is None:
+			return {}
+		yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+		result = urlopen(yql_url).read()
+		data = json.loads(result)
+		res = makeWebhookResult(data)
+	elif req.get("result").get("action") == "Repeat":
+		return {
+			"speech": speech,
+			"displayText": speech,
+			# "data": data,
+			# "contextOut": [],
+			"source": "apiai-weather-webhook-sample"
+		}
     return res
 
 
