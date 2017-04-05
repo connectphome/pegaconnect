9996c130f20a27cb3ba847876609ec13fffe4860
connectphome <accenturepega@gmail.com>
2017-04-05 14:51:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,19 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") == "GoogleHome":
-        speech = "Today in "
-        print("Response:")
-        print(speech)
-
-        return {
-			"speech": speech,
-			"displayText": speech,
-			# "data": data,
-			# "contextOut": [],
-			"source": "apiai-weather-webhook-sample"
-		}
-	elif req.get("result").get("action") == "yahooWeatherForecast":
+	if req.get("result").get("action") == "yahooWeatherForecast":
 		baseurl = "https://query.yahooapis.com/v1/public/yql?"
 		yql_query = makeYqlQuery(req)
 		if yql_query is None:
