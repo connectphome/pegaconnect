802365bc82668359d1750501a505ee6173e096ed
connectphome <accenturepega@gmail.com>
2017-04-07 16:57:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,70 +24,77 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+	req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+	print("Request:")
+	print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+	res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+	res = json.dumps(res, indent=4)
+	# print(res)
+	r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
 
 def basic_authorization(user, password):
 	s = user + ":" + password
-	return "Basic " + s.enconde("base64").rstrip()
+	return "Basic " + s.encode("base64").rstrip()
 
-def processRequest(req):
-	url = "http://acc-pw17.pegatsdemo.com:8080/prweb/PRHTTPService/HomeAISmartHomeIntAPIAI2/Services/ProcessData"
-	params = {'type': "GoogleHome"}
-	req = urllib2.Request(url,headers = {"Authorization": basic_authorization('bdonnelly','rules'), "Content-Type": "application/json", "Accept": "*/*",}, data = json.dumps(params))
-	result = urllib2.urlopen(req)	
-	return makeWebhookResult(result.read())
+def processRequest():
 
 
-	# if req.get("result").get("action") == "GoogleHome":
-	# baseurl = "http://acc-pw17.pegatsdemo.com:8080/prweb/PRHTTPService/HomeAISmartHomeIntAPIAI2/Services/ProcessData?"
-	# yql_query = makeYqlQuery(req)
-		# if yql_query is None:
-		# return {}
-	# yql_url = baseurl + urlencode({'type': yql_query}) + "&format=json"
-	# result = urlopen(yql_url).read()
-	# data = json.loads(result)
-	# res = makeWebhookResult(data)
+	url = "http://acc-pw17.pegatsdemo.com:8080/prweb/PRHTTPService/HomeAISmartHomeIntAPIAI2/Services/ProcessData?type=GoogleHome"
+	params = {}
+	req = urllib2.Request(url,  json.dumps(params), headers={"Authorization": basic_authorization('bdonnelly', 'rules'),
+			"Content-Type": "application/json", "Accept": "*/*", })
+	#print(json.dumps(params))
+	result = urllib2.urlopen(req)
+	v = result.read()
+	data = json.loads(v)
+	return makeWebhookResult(data)
+
+
+
+# if req.get("result").get("action") == "GoogleHome":
+# baseurl = "http://acc-pw17.pegatsdemo.com:8080/prweb/PRHTTPService/HomeAISmartHomeIntAPIAI2/Services/ProcessData?"
+# yql_query = makeYqlQuery(req)
+# if yql_query is None:
+# return {}
+# yql_url = baseurl + urlencode({'type': yql_query}) + "&format=json"
+# result = urlopen(yql_url).read()
+# data = json.loads(result)
+# res = makeWebhookResult(data)
 # return res
 
 
 
 def makeYqlQuery(req):
 
-    return "GoogleHome"
+	return "GoogleHome"
 
 
 def makeWebhookResult(data):
-    if data.get('Device') is None:
-        speech = "Nothing gotten"  
-    else:
-        speech = "Testing Pega"
+	if data.get("Device") is None:
+		speech = "Nothing gotten"
+		print(speech)
+	else:
+		print("else")
+		speech = "Testing Pega"
 
-    print("Response:")
-    print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
+	return {
+		"speech": speech,
+		"displayText": speech,
+		# "data": data,
+		# "contextOut": [],
+		"source": "apiai-weather-webhook-sample"
+		}
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+port = int(os.getenv('PORT', 5000))
 
-    print("Starting app on port %d" % port)
+print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+app.run(debug=False, port=port, host='0.0.0.0')
